3e94f212d1d203110e31b2f37427c9e81270d150
ovclid <53159064+ovclid@users.noreply.github.com>
2020-11-22 19:49:17 +0900

    Add files via upload

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i < 100; i++) // 0부터 99까지 증가하면서 100번 반복
+    {
+        printf("%d Hello, world!\n", i);
+    }
+
+    printf("hello world");
+}
